76d57b2fefce900e42dc5843bbe6913879a24c8a
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-04-13 19:22:30 +0300

    ff

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,7 @@ function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio && bio.length > 1 ? <p>{bio}</p> : null}
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {bio && <p>{bio}</p>}
       <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
